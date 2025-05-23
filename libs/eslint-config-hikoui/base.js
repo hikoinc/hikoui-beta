6494b639c5eb422a6ace46cc7fadeab40f5f26f6
Joe Takara <44985154+joetakara@users.noreply.github.com>
2025-05-23 08:42:26 +0700

    chore(repo): initial commit (#2)
    * chore(repo): initial commit
    
    * chore(config): update base branch from 'canary' to 'main' in configuration files
    
    * chore(workflows): standardize branch definitions
    
    * chore(package): update node.js engine requirement
    
    * refactor(eslint): reorder and organize import statements
    
    * fix(eslint): add missing newline for improved readability
    
    * chore(workflows): remove redundant from release.yml
    
    * fix(eslint): add onlywarn plugin to configuration and remove redundant plugin entry
    
    * chore(workflows): add setup environment step

---
+++ libs/eslint-config-hikoui/base.js
@@ -0,0 +1,28 @@
+import eslintConfigPrettier from "eslint-config-prettier";
+import js from "@eslint/js";
+import onlyWarn from "eslint-plugin-only-warn";
+import tseslint from "typescript-eslint";
+import turboPlugin from "eslint-plugin-turbo";
+
+/**
+ * A shared ESLint configuration for the repository.
+ *
+ * @type {import("eslint").Linter.Config[]}
+ * */
+export const config = [
+  js.configs.recommended,
+  eslintConfigPrettier,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      turbo: turboPlugin,
+      onlyWarn: onlyWarn,
+    },
+    rules: {
+      "turbo/no-undeclared-env-vars": "error",
+    },
+  },
+  {
+    ignores: ["dist/**"],
+  },
+];
