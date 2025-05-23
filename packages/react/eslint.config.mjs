dd13998bc70e9fe6517a7ae6f217555bc18b578a
Joe Takara <anuchit.boonsom@gmail.com>
2025-05-24 03:12:23 +0700

    chore(config): remove eslint-plugin-prettier from package dependencies

--- packages/react/eslint.config.mjs
+++ packages/react/eslint.config.mjs
@@ -2,7 +2,6 @@ import configPrettier from "eslint-config-prettier/flat";
 import globals from "globals";
 import js from "@eslint/js";
 import pluginJsxA11y from "eslint-plugin-jsx-a11y";
-import pluginPrettier from "eslint-plugin-prettier";
 import pluginReact from "eslint-plugin-react";
 import pluginReactHooks from "eslint-plugin-react-hooks";
 import pluginSonarjs from "eslint-plugin-sonarjs";
