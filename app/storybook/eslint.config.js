03fd40e232eab477cd01179765f6ab59b6afa28a
Joe Takara <anuchit.boonsom@gmail.com>
2025-05-24 06:09:00 +0700

    chore(config): enhance eslint configuration

--- app/storybook/eslint.config.js
+++ app/storybook/eslint.config.js
@@ -1,27 +1,30 @@
-import js from "@eslint/js";
 import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
+import js from "@eslint/js";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import { configs as reactHooksConfigs } from "eslint-plugin-react-hooks";
 
 export default tseslint.config(
   {
     ignores: ["dist"],
   },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, reactHooksConfigs["recommended-latest"]],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: "latest",
       globals: globals.browser,
     },
     plugins: {
-      "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true,
+        },
+      ],
     },
   },
 );
