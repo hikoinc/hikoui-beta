2c6de2f9fde25846dce9ca36f9ef2fc2e963a629
Joe Takara <anuchit.boonsom@gmail.com>
2025-05-23 08:32:56 +0700

    refactor(eslint): reorder and organize import statements

--- libs/eslint-config-hikoui/react.js
+++ libs/eslint-config-hikoui/react.js
@@ -1,17 +1,15 @@
-import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
-import tseslint from "typescript-eslint";
-import pluginReactHooks from "eslint-plugin-react-hooks";
-import pluginReact from "eslint-plugin-react";
 import globals from "globals";
-import { config as baseConfig } from "./base.js";
+import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import tseslint from "typescript-eslint";
 
 /**
  * A custom ESLint configuration for libraries that use React.
  *
  * @type {import("eslint").Linter.Config[]} */
 export const config = [
-  ...baseConfig,
   js.configs.recommended,
   eslintConfigPrettier,
   ...tseslint.configs.recommended,
