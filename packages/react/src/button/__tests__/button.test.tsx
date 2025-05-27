84a10345b48ae0c6a89f09bfe6dd556ae82f84f1
Joe Takara <anuchit.boonsom@gmail.com>
2025-05-27 13:44:54 +0700

    test(button): add unit tests for Button component functionality

--- packages/react/src/button/__tests__/button.test.tsx
+++ packages/react/src/button/__tests__/button.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 
-import Button from "./button";
+import Button from "../button";
 
 describe("Button", () => {
   afterEach(() => {
