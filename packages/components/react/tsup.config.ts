f9ae185e4a5f410ed44f6770da48c95905db252c
Joe Takara <anuchit.boonsom@gmail.com>
2025-05-30 21:02:32 +0700

    chore(tsup): enable watch mode in tsup configuration

--- packages/components/react/tsup.config.ts
+++ packages/components/react/tsup.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   format: ["cjs", "esm"],
   clean: true,
   target: "es2022",
+  watch: true,
   dts: true,
   banner: {
     js: '"use client";',
--- packages/core/system/tsup.config.ts
+++ packages/core/system/tsup.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
   clean: true,
+  watch: true,
   target: "es2022",
   dts: true,
 });
