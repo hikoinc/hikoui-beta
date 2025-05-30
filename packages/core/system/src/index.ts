2fa2352fe9316fdbc30507f0f7f40fbf7d60f166
Joe Takara <anuchit.boonsom@gmail.com>
2025-05-30 19:35:41 +0700

    feat(forward-ref): add forwardRef implementation and types

--- packages/core/system/src/index.ts
+++ packages/core/system/src/index.ts
@@ -1,2 +1,5 @@
 export { default as filterHTMLAttributes } from "./filter-html-attributes";
 export type * from "./filter-html-attributes";
+
+export { default as forwardRef } from "./forward-ref";
+export type * from "./forward-ref";
