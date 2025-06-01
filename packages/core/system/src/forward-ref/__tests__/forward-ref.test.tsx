840f0293011460b8eae3af2abfd7d5c962f197bc
Joe Takara <anuchit.boonsom@gmail.com>
2025-06-01 13:05:29 +0700

    test(forward-ref): simplify and enhance unit tests

--- packages/core/system/src/forward-ref/__tests__/forward-ref.test.tsx
+++ packages/core/system/src/forward-ref/__tests__/forward-ref.test.tsx
@@ -7,32 +7,26 @@ describe("forwardRef", () => {
   it("creates a forwardRef component correctly", () => {
     const TestComponent = forwardRef<{ text: string }, "div">((props, ref) => {
       const { text, ...rest } = props;
-
       return (
         <div ref={ref} {...rest}>
           {text}
         </div>
       );
     });
-
     render(<TestComponent text="Hello World" data-testid="test" />);
-
     expect(screen.getByTestId("test")).toHaveTextContent("Hello World");
   });
 
   it("forwards ref correctly", () => {
     const TestComponent = forwardRef<{ text: string }, "div">((props, ref) => {
       const { text, ...rest } = props;
-
       return (
         <div ref={ref} {...rest}>
           {text}
         </div>
       );
     });
-
     let refElement: HTMLDivElement | null = null;
-
     render(
       <TestComponent
         text="Hello World"
@@ -42,7 +36,6 @@ describe("forwardRef", () => {
         }}
       />,
     );
-
     expect(refElement).toBeInstanceOf(HTMLDivElement);
     expect(refElement).toHaveTextContent("Hello World");
   });
