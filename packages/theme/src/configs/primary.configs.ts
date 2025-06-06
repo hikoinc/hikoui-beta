e3a8a1337655568494889d2e5664fd18a12f9550
Joe Takara <anuchit.boonsom@gmail.com>
2025-06-06 16:43:11 +0700

    refactor(chore): improve code quality

--- packages/theme/src/configs/primary.configs.ts
+++ packages/theme/src/configs/primary.configs.ts
@@ -3,18 +3,19 @@ import type { Config, Format } from "style-dictionary/types";
 
 export const cssVariablesFormat: Format = {
   name: "css/variables",
-  format({ dictionary }) {
-    const { allTokens } = dictionary;
+  format(context) {
+    const { allTokens } = context.dictionary;
+
     const commonTokens = _.filter(allTokens, (token) => _.first(token.path) === "common");
     const darkTokens = _.filter(allTokens, (token) => _.first(token.path) === "dark");
 
     const commonValues = _.reduce(
       commonTokens,
-      (acc, token) => {
+      (accumulator, token) => {
         const path = _.join([token.$type, ..._.chain(token.path).slice(2).value()], "-");
-        const value = token.original.$value as string;
+        const value = token.original.$value;
 
-        return _.assign({}, acc, {
+        return _.assign({}, accumulator, {
           [path]: value,
         });
       },
@@ -23,11 +24,11 @@ export const cssVariablesFormat: Format = {
 
     const darkValues = _.reduce(
       darkTokens,
-      (acc, token) => {
+      (accumulator, token) => {
         const path = _.join([token.$type, ..._.chain(token.path).slice(2).value()], "-");
-        const value = token.original.$value as string;
+        const value = token.original.$value;
 
-        return _.assign({}, acc, {
+        return _.assign({}, accumulator, {
           [path]: value,
         });
       },
